eb34134c49dc400538431eca775e66a1bdcc34b8
rpahel <raphael.dacosta.pro@gmail.com>
2023-03-04 18:12:34 +0100

    Setup Laser Platform

--- Assets/Platforms/Scripts/PlatformLaser.cs
+++ Assets/Platforms/Scripts/PlatformLaser.cs
@@ -5,12 +5,49 @@ public class PlatformLaser : Platform
     [Header("Laser settings")]
     [SerializeField, Tooltip("How much damage the laser deals per second.")]
     private float _damagePerSecond;
+    [SerializeField, Tooltip("Max distance the laser can travel."), Range(10, 100)]
+    private float _laserRange;
+    [SerializeField, Range(0, 1)]
+    private float _laserWidth;
+
+    protected LineRenderer _lineRenderer;
+    protected RaycastHit2D _hit;
 
     //=========================================================
 
-    /// <summary> Shoot a laser towards the direction the platform is facing. </summary>
-    protected void ShootLaser()
+    private void Awake()
     {
-        // TODO
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.startWidth = _laserWidth;
+        _lineRenderer.endWidth = _laserWidth;
+    }
+
+    protected void ShootRaycast(Vector2 origin, Vector2 direction)
+    {
+        _hit = Physics2D.Raycast(origin, direction, _laserRange);
+    }
+
+    protected void DrawLaser(Vector2 origin, Vector2 direction)
+    {
+        _lineRenderer.positionCount = 2;
+        _lineRenderer.SetPosition(0, origin);
+        if (_hit)
+            _lineRenderer.SetPosition(1, _hit.point);
+        else
+            _lineRenderer.SetPosition(1, origin + direction * _laserRange);
+    }
+
+    private void LateUpdate()
+    {
+        if(this is not PlatformMirror)
+        {
+            ShootRaycast(transform.position, transform.up);
+            DrawLaser(transform.position, transform.up);
+        }
+
+        if (_hit && _hit.collider.CompareTag("Mirror"))
+        {
+
+        }
     }
 }
