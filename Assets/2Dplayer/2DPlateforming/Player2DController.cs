6a34db502deaa5dc66a77026e447a1149b123397
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2023-03-05 17:24:32 +0100

    maj player ajout spoawnb de pf

--- Assets/2Dplayer/2DPlateforming/Player2DController.cs
+++ Assets/2Dplayer/2DPlateforming/Player2DController.cs
@@ -1,26 +1,30 @@
 using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using DG.Tweening;
-using DG.Tweening.Core.Easing;
 
 public enum PFenum
 {
-    Basic=1,
+    None = 0,
+    Basic =1,
     Bounce=2,
     Boost=3,
     Lazer=4,
     Mirror=5,
     Gravity=6,
-    None=0
+
 }
 public class Player2DController : MonoBehaviour
 {
     public static Player2DController instance;
-    private PFenum choosenPF;
+    public PFenum choosenPF;
+    public GameObject[] pfPrefabs;
+    public GameObject mouseFollow;
+    public float rotationMultiplyer;
     [SerializeField] private GameObject pfContainer;
     public float life;
     public float damage;
+    private bool canRotate;
     [Header("Stats")]
     [SerializeField] private float maxlife;
     [SerializeField] private float speed;
@@ -35,14 +39,21 @@ public class Player2DController : MonoBehaviour
 
     [Header("Jump")]
     [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isJumping;
     [SerializeField] private AnimationCurve jumpCurve;
     [SerializeField] private float jumpDuration = 0.5f;
     [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float gravityMultiplyer;
+    [SerializeField] private float fallGravity;
+    [SerializeField] float coyoteTime;
+    private float coyoteTimeCounter;
+    [SerializeField] private float jumpBufferTime;
+    private float jumpBufferCounter;
+
     private CapsuleCollider2D col;
     private Coroutine _jumpCoroutine;
     private float duration;
     private float timer;
-    [SerializeField] private float fallGravity;
 
     private Vector2 direction;
     [SerializeField] private float acceleration;
@@ -50,8 +61,8 @@ public class Player2DController : MonoBehaviour
     [SerializeField] private float velPower;
     [SerializeField] private float frictionAnmount;
     [SerializeField] private float groundedSize;
-
-
+    Vector3 mousePosition;
+    GameObject newPF;
     private Vector2 velocity;
     void Start()
     {
@@ -66,21 +77,68 @@ public class Player2DController : MonoBehaviour
         col = GetComponent<CapsuleCollider2D>();
         baseSpeed=speed;
         pfContainer.SetActive(false);
+        
     }
     public void SelectPF(int id)
     {
         choosenPF = (PFenum)id;
-        //switch (id)
-        //{
-        //    case 1:choosenPF=PFenum.Basic;break;
-        //    case 2:choosenPF=PFenum.Bounce;break;
-        //    case 3:choosenPF=PFenum.Gravity;break;
-        //    case 4:choosenPF=PFenum.Lazer;break;
-        //    case 5:choosenPF=PFenum.Mirror;break;
-        //    case 6:choosenPF=PFenum.Boost;break;
-        //    default:
-        //        break;
-        //}
+        Debug.Log("PFselect");
+    }
+    public void spawnPF(InputAction.CallbackContext context)
+    {
+        if(context.started)
+            newPF = Instantiate(pfPrefabs[(int)choosenPF], mouseFollow.transform.position, Quaternion.identity);
+        if (context.performed)
+        {
+            if(Mouse.current.leftButton.IsPressed())
+            {
+                canRotate = true;
+                Debug.Log("test");
+            }
+
+        }
+        if(context.canceled)
+        {
+            canRotate = false;
+        }
+
+    }
+    private void Update()
+    {
+        FollowMouse();
+
+        if (GroundCheck())
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+        jumpBufferCounter-= Time.deltaTime;
+        
+        if(canRotate)
+        {
+            
+            //newPF.transform.LookAt(mousePosition);
+            Vector3 dir = newPF.transform.InverseTransformPoint(mousePosition);
+            var angle = Mathf.Atan2(dir.y, dir.x)*Mathf.Rad2Deg-90;
+            newPF.transform.Rotate(0, 0, angle);
+
+            //float dist = Vector3.Distance(newPF.transform.position, mousePosition);
+            //Debug.Log(dist);
+            //if(newPF.transform.position.x> mousePosition.x)
+            //    newPF.transform.rotation = Quaternion.Euler(0, 0, dist* rotationMultiplyer);
+            //else
+            //    newPF.transform.rotation = Quaternion.Euler(0, 0, dist * -rotationMultiplyer);
+        }
+    }
+    public void FollowMouse()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
+        mouseFollow.transform.position = mousePosition;
+
     }
     private void FixedUpdate()
     {
@@ -91,17 +149,15 @@ public class Player2DController : MonoBehaviour
         float speedDif = targetSpeed - rb.velocity.x;
         float accelRate = (Mathf.Abs(targetSpeed) > 0.001f) ? acceleration : decceleration;
         float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
-
-
-            rb.AddForce(movement * Vector2.right);
+        rb.AddForce(movement * Vector2.right);
         if (direction.x == 0 && rb.velocity.x != 0)
         {
             float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), Mathf.Abs(frictionAnmount));
             amount *= Mathf.Sign(rb.velocity.x);
             rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
         }
-
     }
+
     public void MovePlayer(InputAction.CallbackContext context)
     {
         velocity = context.ReadValue<Vector2>();
@@ -120,8 +176,8 @@ public class Player2DController : MonoBehaviour
         if (context.performed)
         {
             pfContainer.SetActive(true);
-            Time.timeScale = 0.2f;
-            //pfContainer.GetComponent<PFUIContainer>().AnimeUI();
+            Time.timeScale = 0.1f;
+            pfContainer.GetComponent<PFUIContainer>().AnimeUI();
         }
         else
         {
@@ -148,33 +204,50 @@ public class Player2DController : MonoBehaviour
 
         return hit;
     }
+    public bool HeadCheck()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, col.bounds.max.y), gameObject.transform.up, groundedSize);
+        Debug.DrawRay(new Vector2(transform.position.x, col.bounds.max.y), gameObject.transform.up * groundedSize, Color.magenta);
+
+        return hit;
+    }
     public void jump(InputAction.CallbackContext context)
-    {   if (GroundCheck())
-            if (context.performed)
-                _jumpCoroutine= StartCoroutine(JumpCoroutine());
-            else if (context.canceled)
+    {
+        if (context.performed)
+        {
+            //jumpBufferCounter = jumpBufferTime;
+            if (coyoteTimeCounter > 0f)
+                _jumpCoroutine = StartCoroutine(JumpCoroutine());
+        }
+        else if (context.canceled)
+        {
+            if (_jumpCoroutine != null)
             {
                 StopCoroutine(_jumpCoroutine);
                 _jumpCoroutine = null;
-                rb.gravityScale = 1;
+                coyoteTimeCounter = 0f;
             }
+
+            rb.gravityScale = fallGravity;
+        }
     }
     private IEnumerator JumpCoroutine()
     {
-        //isJumping = true;
-        
+        isJumping = true;
         float t = 0;
         rb.gravityScale = 0;
         while (t < 1)
         {
             rb.velocity = new Vector2(rb.velocity.x, Mathf.LerpUnclamped(jumpForce, 0, jumpCurve.Evaluate(t)));
             t += Time.fixedDeltaTime / jumpDuration;
+            if (HeadCheck())
+                break;
             yield return new WaitForFixedUpdate();
         }
-        //playerSystemManager.isJumping = false;
-        //playerSystemManager.Rb2D.gravityScale = playerSystemManager.GravityScale;
+        isJumping=false;
         rb.gravityScale = fallGravity;
         _jumpCoroutine = null;
+        coyoteTimeCounter = 0f;
         yield break;
     }
 
--- Assets/PFUIContainer.cs
+++ Assets/PFUIContainer.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+
 public class PFUIContainer : MonoBehaviour
 {
     public List<GameObject> pfChild = new List<GameObject>();
-    
+    //public int id;
     public void AnimeUI()
     {
         StartCoroutine(AnimeCour());
     }
-    
+
     private IEnumerator AnimeCour()
     {
         foreach (GameObject go in pfChild)
@@ -18,8 +19,15 @@ public class PFUIContainer : MonoBehaviour
             
             Sequence mySequence = DOTween.Sequence();
             mySequence.Append(go.transform.DOScale(new Vector3(0,0,0),0));
-            mySequence.Append(go.transform.DOScale(new Vector3(1, 1, 1), 1f));
-            yield return new WaitForSeconds(0.1f);
+            mySequence.Append(go.transform.DOScale(new Vector3(0.65f, 0.65f, 0.65f), 0.05f).SetEase(Ease.OutBounce));
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    private void OnDisable()
+    {
+        foreach (GameObject go in pfChild)
+        {
+            go.transform.localScale=new Vector3(0,0,0);
         }
     }
 }
