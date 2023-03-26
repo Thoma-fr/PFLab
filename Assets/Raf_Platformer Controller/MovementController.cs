91b1d1f1c0c22501393fd703ef70dc795c6aa13b
rpahel <raphael.dacosta.pro@gmail.com>
2023-03-26 14:38:35 +0200

    New Jump

--- Assets/Raf_Platformer Controller/MovementController.cs
+++ Assets/Raf_Platformer Controller/MovementController.cs
@@ -1,20 +1,39 @@
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    [Header("Jump")]
-    [SerializeField, Range(0, 2f)]
-    private float _groundCheckDistance;
-
     [Header("Movement")]
     [SerializeField]
     private float _maxSpeed;
+    [SerializeField]
+    private float _minSpeed;
     [SerializeField]
     private float _accelerationStrength;
     [SerializeField]
     private float _airAccelerationFactor;
     [SerializeField]
     private float _deccelerationStrength;
+
+    [Header("Jump")]
+    [SerializeField, Range(0, 2f)]
+    private float _groundCheckDistance;
+    [SerializeField, Range(0, 2f)]
+    private float _headCheckDistance;
+    [SerializeField]
+    private AnimationCurve _jumpVelocityCurve;
+    [SerializeField]
+    private float _jumpStrength;
+    [SerializeField]
+    private float _minJumpDuration;
+    [SerializeField]
+    private float _maxJumpDuration;
+    [SerializeField]
+    private float _jumpDurationMultiplier;
+    private float _defaultGravityScale;
+    private float _currentJumpDuration;
+    private Coroutine _jumpCoroutine;
 
     private Rigidbody2D _rb2D;
 
@@ -23,9 +42,10 @@ public class MovementController : MonoBehaviour
     private void Awake()
     {
         _rb2D = GetComponent<Rigidbody2D>();
+        _defaultGravityScale = _rb2D.gravityScale;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if(Input.anyKey) // A modifier pour le passage vers le new input system
         {
@@ -41,46 +61,123 @@ public class MovementController : MonoBehaviour
         }
         else
         {
-            if (_rb2D.velocity.sqrMagnitude > 0 && GroundCheck())
+            if (_rb2D.velocity.sqrMagnitude > 0 && GroundCheck(~LayerMask.GetMask("Bouncing Platform", "Speed Platform")))
                 Decceleration();
         }
 
-        if(GroundCheck())
-            CapSpeed();
+        if(GroundCheck(~LayerMask.GetMask("Bouncing Platform", "Speed Platform")))
+            ClampSpeed(_maxSpeed);
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if (GroundCheck(~LayerMask.GetMask("Bouncing Platform")) && _jumpCoroutine == null)
+                _jumpCoroutine = StartCoroutine(JumpCoroutine());
+            else if (_jumpCoroutine != null)
+                IncreaseJumpDuration(Time.deltaTime * _jumpDurationMultiplier);
+        }
     }
 
-    private bool GroundCheck()
+    private bool GroundCheck(LayerMask layerMask)
     {
-        RaycastHit2D hit = Physics2D.CircleCast(transform.position, .5f, Vector2.down, _groundCheckDistance, ~LayerMask.GetMask("Bouncing Platform"));
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, .5f, Vector2.down, _groundCheckDistance, layerMask);
 
+#if UNITY_EDITOR
         if (hit)
             CoolDebugs.CoolDebugs.DrawWireSphere((Vector2)transform.position + _groundCheckDistance * Vector2.down, .5f, Color.green, Time.fixedDeltaTime);
         else
             CoolDebugs.CoolDebugs.DrawWireSphere((Vector2)transform.position + _groundCheckDistance * Vector2.down, .5f, Color.red, Time.fixedDeltaTime);
+#endif
 
         return hit;
     }
-    private void CapSpeed()
+
+    private bool HeadCheck()
     {
-        if(_rb2D.velocity.sqrMagnitude > _maxSpeed * _maxSpeed)
-            _rb2D.velocity = _maxSpeed * _rb2D.velocity.normalized;
+        return Physics2D.CircleCast(transform.position, .5f, Vector2.up, _headCheckDistance);
+    }
+
+    public void ClampSpeed(float maxSpeed)
+    {
+        if(_rb2D.velocity.sqrMagnitude > maxSpeed * maxSpeed)
+            _rb2D.velocity = maxSpeed * _rb2D.velocity.normalized;
     }
 
     private void Decceleration()
     {
-        _rb2D.velocity -= _deccelerationStrength * _rb2D.velocity.normalized;
+        _rb2D.velocity -= _deccelerationStrength * Time.deltaTime * _rb2D.velocity.normalized;
 
-        if (_rb2D.velocity.sqrMagnitude < 0.5f)
-        {
+        if (_rb2D.velocity.sqrMagnitude < _minSpeed * _minSpeed)
             _rb2D.velocity = Vector2.zero;
-        }
     }
 
     private void Acceleration(Vector2 direction)
     {
-        if (GroundCheck())
-            _rb2D.velocity += _accelerationStrength * direction;
+        if (GroundCheck(LayerMask.GetMask("Speed Platform")))
+            return;
+        
+        if (GroundCheck(~LayerMask.GetMask("Bouncing Platform")))
+            _rb2D.velocity += _accelerationStrength * Time.deltaTime * direction;
         else
-            _rb2D.velocity += _accelerationStrength * _airAccelerationFactor * direction;
+            _rb2D.velocity += _accelerationStrength * _airAccelerationFactor * Time.deltaTime * direction;
     }
+    
+    private IEnumerator JumpCoroutine()
+    {
+        _defaultGravityScale = _rb2D.gravityScale;
+        _rb2D.gravityScale = 0;
+        _currentJumpDuration = _minJumpDuration;
+
+        float t = 0;
+        while(t < _currentJumpDuration)
+        {
+            if (HeadCheck())
+                break;
+
+            _rb2D.velocity = new Vector2(_rb2D.velocity.x, _jumpStrength * _jumpVelocityCurve.Evaluate(t / _currentJumpDuration));
+
+            t += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+
+        StopJumpCoroutine();
+        yield break;
+    }
+
+    private void IncreaseJumpDuration(float amount)
+    {
+        _currentJumpDuration = Mathf.Clamp(_currentJumpDuration + amount, _minJumpDuration, _maxJumpDuration);
+    }
+
+    public void StopJumpCoroutine() // Utile pour la platforme de gravity
+    {
+        if( _jumpCoroutine != null )
+            StopCoroutine(_jumpCoroutine);
+
+        _rb2D.gravityScale = _defaultGravityScale;
+        _jumpCoroutine = null;
+    }
+
+#if UNITY_EDITOR
+    // =====================================================================
+
+    private void OnDrawGizmosSelected()
+    {
+        if(_maxJumpDuration < _minJumpDuration)
+            _maxJumpDuration = _minJumpDuration;
+
+        GUIStyle labels = new GUIStyle();
+        labels.alignment = TextAnchor.MiddleCenter;
+        labels.normal.textColor = Color.black;
+        labels.fontStyle = FontStyle.Bold;
+        labels.fontSize = 20;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere((Vector2)transform.position + _groundCheckDistance * Vector2.down, .5f);
+        Handles.Label((Vector2)transform.position + _groundCheckDistance * Vector2.down, "Ground", labels);
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere((Vector2)transform.position + _headCheckDistance * Vector2.up, .5f);
+        Handles.Label((Vector2)transform.position + _headCheckDistance * Vector2.up, "Head", labels);
+    }
+#endif
 }
